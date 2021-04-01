c71fdd143a7ebcf5bf65d1421636dc475cffa3fb
DDing77 <smg76262585@gmail.com>
2021-04-01 23:39:23 +0900

    로그인기능 초반

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -29,7 +30,37 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-})
+});
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')){
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) return next(err);
+            user.password = hash;
+            // Store hash in your password DB.
+            next();
+        });
+    }) 
+    } else{
+        next();
+    };
+});
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 1234567 암호화된 비밀번호 $2b$10$CFX9nR93AEPaDcSvrjbYz.u4DanCGTObScd0UfF1ApQQwF32/0WZK
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+
+
+
+};
 
 const User = mongoose.model('User',userSchema); // 모델화
 
